4d7b4e633b3eeda75fe6cc5de858af6e6492eff3
Baocheng Wang <baocheng0121@gmail.com>
2026-01-20 01:50:53 +1100

    - [chore] decoupled project-level styles to global.css in case App.css too crowded

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppShell from './components/AppShell';
 import './App.css';
+import '-/styles/global.css';
 
 export default function App() {
   return <AppShell />;
