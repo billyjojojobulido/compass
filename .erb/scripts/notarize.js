1aa15deec5f87131951128ec79800fbcbec6940c
Baocheng Wang <baocheng0121@gmail.com>
2026-03-21 21:26:05 +1100

    - [admin] updated notralize.js to update the notralize process after signing

--- .erb/scripts/notarize.js
+++ .erb/scripts/notarize.js
@@ -3,36 +3,36 @@ const { build } = require('../../package.json');
 
 exports.default = async function notarizeMacos(context) {
   const { electronPlatformName, appOutDir } = context;
+
   if (electronPlatformName !== 'darwin') {
     return;
   }
 
-  if (process.env.CI !== 'true') {
-    console.warn('Skipping notarizing step. Packaging is not running in CI');
-    return;
-  }
-
   if (
-    !(
-      'APPLE_ID' in process.env &&
-      'APPLE_ID_PASS' in process.env &&
-      'APPLE_TEAM_ID' in process.env
-    )
+    !process.env.APPLE_ID ||
+    !process.env.APPLE_APP_SPECIFIC_PASSWORD ||
+    !process.env.APPLE_TEAM_ID
   ) {
     console.warn(
-      'Skipping notarizing step. APPLE_ID, APPLE_ID_PASS, and APPLE_TEAM_ID env variables must be set',
+      'Skipping notarizing step. APPLE_ID, APPLE_APP_SPECIFIC_PASSWORD, and APPLE_TEAM_ID env variables must be set',
     );
     return;
   }
 
   const appName = context.packager.appInfo.productFilename;
 
+  console.log('Notarizing macOS app...');
+  console.log('appBundleId:', build.appId);
+  console.log('appPath:', `${appOutDir}/${appName}.app`);
+
   await notarize({
     tool: 'notarytool',
     appBundleId: build.appId,
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_ID_PASS,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
     teamId: process.env.APPLE_TEAM_ID,
   });
+
+  console.log('Notarization complete.');
 };
